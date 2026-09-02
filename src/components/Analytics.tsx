b815d6ddc00bb9995d817dee5bc159dd22930540
benduran <stratodyne@gmail.com>
2026-09-02 19:03:29 +0100

    chore: only render analytics when running in prod mode

--- src/components/Analytics.tsx
+++ src/components/Analytics.tsx
@@ -1,4 +1,9 @@
 export function Analytics() {
+  // biome-ignore lint/style/noProcessEnv: needed to determine if we should render the script
+  const isProd = process.env.NODE_ENV === 'production';
+
+  if (!isProd) return null;
+
   return (
     <script
       data-website-id="16edb034-e99c-4463-a190-492709c59210"
