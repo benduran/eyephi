9682ddc08753df830cb1c60185ca43ac536f8ba9
benduran <stratodyne@gmail.com>
2026-09-02 19:01:28 +0100

    chore: added anonmyous analytics

---
+++ src/components/Analytics.tsx
@@ -0,0 +1,9 @@
+export function Analytics() {
+  return (
+    <script
+      data-website-id="16edb034-e99c-4463-a190-492709c59210"
+      defer
+      src="https://yummy.benduran.com/script.js"
+    />
+  );
+}
